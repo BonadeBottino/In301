bec00c9fcd832b2b567a3b84dbafbe9173ef3ea7
Utilisateur <user@debian.arcahe.ovh>
2017-11-24 16:29:14 +0100

    "modification 24/11/2017"

--- Projet/afficher.c
+++ Projet/afficher.c
@@ -1,26 +1,55 @@
 #include <uvsqgraphics.h>
 
 #include "mes_types.h"
+#include "lire_ecrire.h"
 
 
-void initialiser_affichage(SLIDER S) {
-	init_graphics (50*S.L,50*S.H);
+void initialiser_affichage(SLIDER S) 
+{
+	init_graphics (CASE*S.L,CASE*S.H);
 }
 
-void afficher_grille(SLIDER S) {
+void afficher_grille(SLIDER S) 
+{
+	int i ; 
+	POINT p1,p2 ; 
+	
+	p1.x=0 ; p1.y=0 ;
+	p2.x=0 ; p2.y=S.H*CASE ;
+	
+	for(i=0 ; i<S.L ; i++)
+	{
+		p1.x+=CASE ; 
+		p2.x+=CASE ;
+		draw_line(p1,p2,red) ; 
+	}
+	
+	p1.x=0 ; p1.y=0 ;
+	p2.x=S.L*CASE ; p2.y=0 ;
+	
+	for(i=0 ; i<S.H ; i++)
+	{
+		p1.y+=CASE ; 
+		p2.y+=CASE ;
+		draw_line(p1,p2,blue) ; 
+	}
 }
 
-void afficher_murs(SLIDER S) {
+void afficher_murs(SLIDER S) 
+{
 }
 
-void afficher_le_slider(SLIDER S) {
+void afficher_le_slider(SLIDER S) 
+{
 }
 
-void afficher_sortie(SLIDER S) {
+void afficher_sortie(SLIDER S) 
+{
 }
 
 
-void afficher_slider (SLIDER S) {
+void afficher_slider (SLIDER S) 
+{
 	afficher_grille(S);
 	afficher_murs(S);
 	afficher_le_slider(S);
@@ -28,6 +57,7 @@ void afficher_slider (SLIDER S) {
 }
 
 
-void finir_affichage(SLIDER S) {
+void finir_affichage(SLIDER S) 
+{
 	wait_escape();
 }
